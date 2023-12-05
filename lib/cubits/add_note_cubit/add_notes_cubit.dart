743788271ba641b58298953744afc49601f25b7b
Hishamk99 <hishamkamel370@gmail.com>
2023-12-05 12:32:43 +0200

    create notes state

--- lib/cubits/add_note_cubit/add_notes_cubit.dart
+++ lib/cubits/add_note_cubit/add_notes_cubit.dart
@@ -1,7 +1,6 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-//import 'package:meta/meta.dart';
 import 'package:notes_project/constants.dart';
 import 'package:notes_project/models/note_model.dart';
 part 'add_notes_state.dart';
